ab709200704c9e320645c5e9874492c523c8d2bb
Mizu <mizu@blackicegames.de>
2017-07-14 14:56:05 +0200

    try mumble connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "Connector.h"
+#include "mumble/MumbleConnector.h"
+#include "ManagedSocket.h"
+#include <string>
+#include <iostream>
 
 int main(){
+	std::string s = "sneaky-ninja.de";
+	s = "localhost";
+	std::cout << "creating connector" << std::endl;
+	Connector* c = new MumbleConnector(s);
+	std::cout << "done" << std::endl;
+	delete c;
 }
