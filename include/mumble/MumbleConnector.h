ad1912f67802d6aa89a114be3deef9aade95c1b4
Mizu <mizu@blackicegames.de>
2017-07-17 22:21:09 +0200

    implementing (input) message handling
    define enum via macro (mumble seems to use this promissingly...)

--- include/mumble/MumbleConnector.h
+++ include/mumble/MumbleConnector.h
@@ -1,8 +1,39 @@
 #pragma once
+#include "mumble.pb.h"
 #include "Connector.h"
 #include "ManagedSSLSocket.h"
 #include <thread>
 #include <iostream>
+
+#define HEADER_SIZE 6
+#define MUMBLE_MESSAGE_TYPES_ALL \
+    MUMBLE_MESSAGE_TYPE(Version) \
+    MUMBLE_MESSAGE_TYPE(UDPTunnel) \
+    MUMBLE_MESSAGE_TYPE(Authenticate) \
+    MUMBLE_MESSAGE_TYPE(Ping) \
+    MUMBLE_MESSAGE_TYPE(Reject) \
+    MUMBLE_MESSAGE_TYPE(ServerSync) \
+    MUMBLE_MESSAGE_TYPE(ChannelRemove) \
+    MUMBLE_MESSAGE_TYPE(ChannelState) \
+    MUMBLE_MESSAGE_TYPE(UserRemove) \
+    MUMBLE_MESSAGE_TYPE(UserState) \
+    MUMBLE_MESSAGE_TYPE(BanList) \
+    MUMBLE_MESSAGE_TYPE(TextMessage) \
+    MUMBLE_MESSAGE_TYPE(PermissionDenied) \
+    MUMBLE_MESSAGE_TYPE(ACL) \
+    MUMBLE_MESSAGE_TYPE(QueryUsers) \
+    MUMBLE_MESSAGE_TYPE(CryptSetup) \
+    MUMBLE_MESSAGE_TYPE(ContextActionModify) \
+    MUMBLE_MESSAGE_TYPE(ContextAction) \
+    MUMBLE_MESSAGE_TYPE(UserList) \
+    MUMBLE_MESSAGE_TYPE(VoiceTarget) \
+    MUMBLE_MESSAGE_TYPE(PermissionQuery) \
+    MUMBLE_MESSAGE_TYPE(CodecVersion) \
+    MUMBLE_MESSAGE_TYPE(UserStats) \
+    MUMBLE_MESSAGE_TYPE(RequestBlob) \
+    MUMBLE_MESSAGE_TYPE(ServerConfig) \
+    MUMBLE_MESSAGE_TYPE(SuggestConfig)
+#undef MUMBLE_MESSAGE_TYPE //undefine to possibly redefine for magical use
 
 class MumbleHeader{
 	public:
@@ -18,32 +49,9 @@ class MumbleHeader{
 		const uint32_t messageLength;
 };
 enum MumbleMessageType{//Maybe macros?
-	Version,
-	UDPTunnel,
-	Authenticate,
-	Ping,
-	Reject,
-	ServerSync,
-	ChannelRemove,
-	ChannelState,
-	UserRemove,
-	UserState,
-	BanList,
-	TextMessage,
-	PermissionDenied,
-	ACL,
-	QueryUsers,
-	CryptSetup,
-	ContextActionModify,
-	ContextAction,
-	UserList,
-	VoiceTarget,
-	PermissionQuery,
-	CodecVersion,
-	UserStats,
-	RequestBlob,
-	ServerConfig,
-	SuggestConfig
+#define MUMBLE_MESSAGE_TYPE(x) x, //define macro to define list
+	MUMBLE_MESSAGE_TYPES_ALL
+#undef MUMBLE_MESSAGE_TYPE //undefine to possibly redefine for magical use
 };
 
 class MumbleConnector: public Connector{
@@ -55,8 +63,24 @@ class MumbleConnector: public Connector{
 		ManagedSSLSocket* socket=NULL;
 		bool receiveLoopRuns=false;
 		std::thread receiveThread;
-		void receiveLoop();
+		void handleReceives();
 		void connect();
 		void auth();
 		void disconnect();
+		void sendProtoMessage(const MumbleMessageType& msgType,const std::string& message);
+		void dispatchMessage(const MumbleHeader& header, const std::string& message);
+
+		void handlePing(MumbleProto::Ping pingMsg);
+		//void handleText(MumbleProto::TextMessage textMsg);
+		void handleCrypto(MumbleProto::CryptSetup cryptMsg);
+		//void handleServerSync(MumbleProto::ServerSync syncMsg);
+		//void handleReject(MumbleProto::Reject rejectMsg);
+		//void handleChannelState(MumbleProto::ChannelState stateMsg);
+		//void handleUserState(MumbleProto::UserState stateMsg);
+		//void handlePermissionDenied(MumbleProto::PermissionDenied deniedMsg);
+		//void handlePermissionQuery(MumbleProto::PermissionQuery queryMsg);
+		//void handleQueryUsers(MumbleProto::QueryUsers queryMsg);
+		//void handleCodec(MumbleProto::CodecVersion codecMsg);
+		//void handleServerConfig(MumbleProto::ServerConfig configMsg);
+		//void handleSuggestConfig(MumbleProto::SuggestConfig configMsg);
 };
--- src/mumble/MumbleConnector.cpp
+++ src/mumble/MumbleConnector.cpp
@@ -1,6 +1,8 @@
 #include "mumble/MumbleConnector.h"
-#include "mumble.pb.h"
+#include <google/protobuf/text_format.h>
 #include <iostream>
+#include <sstream>
+#include <algorithm>
 
 MumbleConnector::MumbleConnector(std::string host,int port){
 	//open socket
@@ -17,6 +19,7 @@ MumbleConnector::MumbleConnector(std::string host,int port){
 }
 
 MumbleConnector::~MumbleConnector(){
+	std::cout << "connector ends" << std::endl;
 	receiveLoopRuns=false;
 	receiveThread.join();
 	if(socket!=NULL){
@@ -28,38 +31,147 @@ MumbleConnector::~MumbleConnector(){
 void MumbleConnector::sendTextMessage(std::string message){
 }
 
-void MumbleConnector::connect(){
-	//version exchange (skippable)
-	const int versionBytes=0b00000000000000010000001000011101;//const version id, connect as current stable (13.07.17)
-	int bufferSize;
-	std::cout << "connected1" << std::endl;
-	MumbleProto::Version myVersion; //MumbleProto::Version myVersion=MumbleProto::Version();
-	myVersion.set_version(versionBytes);
-	bufferSize=myVersion.ByteSize();
-	char buffer[bufferSize];
-	myVersion.SerializeToArray(&buffer,bufferSize);
-	std::cout << "connected2" << std::endl;
-	socket->send(buffer,bufferSize);
-}
-
-void MumbleConnector::receiveLoop(){
+void MumbleConnector::handleReceives(){
 	while(receiveLoopRuns){
-		std::string tmp = socket->receive();
+		std::string tmp=socket->receive();
 		if(tmp.size()>0){
-			std::cout << tmp << std::endl;
+			std::string header_str=tmp.substr(0,HEADER_SIZE);
+			const MumbleHeader* netHeader = reinterpret_cast<const MumbleHeader*>(header_str.c_str());
+			const MumbleHeader header(ntohs(netHeader->getMessageType()),ntohl(netHeader->getMessageLength()));
+			std::string message=tmp.substr(HEADER_SIZE+1,header.getMessageLength());
+			dispatchMessage(header,message);
+		}else{
+				std::cout << "none" << std::endl;
 		}
 	}
 }
 
+void MumbleConnector::connect(){
+	//version exchange (skippable)
+	const int versionBytes=0b00000000000000010000001000011101;//const version id, connect as current stable (13.07.17)
+	const std::string release = "1.2.29";
+	MumbleProto::Version myVersion;
+	std::string buffer;
+	myVersion.set_version(versionBytes);
+	myVersion.set_release(release.c_str());
+	myVersion.SerializeToString(&buffer);
+	sendProtoMessage(MumbleMessageType::Version,buffer);
+	std::cout << "connected2" << std::endl;
+}
+
 void MumbleConnector::auth(){
-	int bufferSize;
 	MumbleProto::Authenticate authMessage;
 	authMessage.set_username("Test");
-	bufferSize=authMessage.ByteSize();
-	char buffer[bufferSize];
-	authMessage.SerializeToArray(&buffer,bufferSize);
-	socket->send(buffer,bufferSize);
+	std::cout << "authing" << std::endl;
+	std::string buffer;
+	authMessage.SerializeToString(&buffer);
+	sendProtoMessage(MumbleMessageType::Authenticate,buffer);
+	std::cout << "authed" << std::endl;
 }
 
 void MumbleConnector::disconnect(){
+}
+
+void MumbleConnector::sendProtoMessage(const MumbleMessageType& msgType,const std::string& message){
+	std::stringstream protoMessage;
+	const MumbleHeader header(msgType,(uint32_t)message.size());
+	header.writeToStreamInNetworkFormat(protoMessage);
+	protoMessage << message;
+	protoMessage.flush();
+	socket->send(protoMessage.str());
+}
+
+void MumbleConnector::dispatchMessage(const MumbleHeader& header, const std::string& message){
+	switch(header.getMessageType()){
+		case MumbleMessageType::Version:{
+			std::clog<< "version: " <<std::endl;
+			MumbleProto::Version v;
+			v.ParseFromString(message);
+			std::string versionDisplay;
+			google::protobuf::TextFormat::PrintToString(v,&versionDisplay);
+			std::clog<< versionDisplay <<std::endl;
+			break;
+		}
+		case MumbleMessageType::Ping:{
+			std::clog<< "ping" <<std::endl;
+			MumbleProto::Ping p;
+			p.ParseFromString(message);
+			std::thread(&MumbleConnector::handlePing,this,std::ref(p)).detach();
+			break;
+		}
+		case MumbleMessageType::Reject:{
+			 std::clog<< "Reject" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::ServerSync:{
+			 std::clog<< "ServerSync" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::ChannelState:{
+			 std::clog<< "ChannelState" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::UserState:{
+			 std::clog<< "UserState" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::TextMessage:{
+			std::clog<< "TextMessage" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::PermissionDenied:{
+			std::clog<< "PermissionDenied" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::QueryUsers:{
+			std::clog<< "QueryUsers" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::CryptSetup:{
+			std::clog<< "CryptSetup" <<std::endl;
+			MumbleProto::CryptSetup cs;
+			cs.ParseFromString(message);
+			handleCrypto(cs);
+			std::thread(&MumbleConnector::handleCrypto,this,std::ref(cs)).detach();
+			break;
+		}
+		case MumbleMessageType::UserList:{
+			std::clog<< "UserList" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::PermissionQuery:{
+			std::clog<< "PermissionQuery" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::CodecVersion:{
+			std::clog<< "CodecVersion" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::ServerConfig:{
+			std::clog<< "ServerConfig" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::SuggestConfig:{
+			std::clog<< "SuggestConfig" <<std::endl;
+			break;
+		}
+		case MumbleMessageType::VoiceTarget: /*future feature?*/ std::clog<< "VoiceTarget" <<std::endl; break;
+		case MumbleMessageType::ChannelRemove: /*probably irrelevant*/ std::clog<< "ChannelRemove" <<std::endl; break;
+		case MumbleMessageType::UserRemove: /*probably irrelevant*/ std::clog<< "UserRemove" <<std::endl; break;
+		case MumbleMessageType::BanList: /*irrelevant*/ std::clog<< "BanList" <<std::endl; break;
+		case MumbleMessageType::ACL: /*irrelevant*/ std::clog<< "ACL" <<std::endl; break;
+		case MumbleMessageType::ContextActionModify: /*irrelevant*/ std::clog<< "ContextActionAdd" <<std::endl; break;
+		case MumbleMessageType::ContextAction: /*irrelevant*/ std::clog<< "ContextAction" <<std::endl; break;
+		case MumbleMessageType::UserStats: /*irrelevant*/ std::clog<< "UserStats" <<std::endl; break;
+		case MumbleMessageType::RequestBlob: /*irrelevant*/ std::clog<< "RequestBlob" <<std::endl; break;
+		case MumbleMessageType::UDPTunnel: /*unused*/ break;
+		case MumbleMessageType::Authenticate: /*should not be recieved by client*/ std::clog<< "received auth message?" <<std::endl; break;
+		default: std::clog<< "dunno what this is" <<std::endl; break;
+	}
+}
+
+void MumbleConnector::handlePing(MumbleProto::Ping pingMsg){
+	std::cout << "send pong?" <<std::endl;
+}
+void MumbleConnector::handleCrypto(MumbleProto::CryptSetup cryptMsg){
 }
