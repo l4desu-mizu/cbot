9b85662b9ab559e4be4102d63d20e4b2f8bee910
Mizu <mizu@blackicegames.de>
2017-07-19 13:16:16 +0200

    add connect method to connector

--- include/Connector.h
+++ include/Connector.h
@@ -5,4 +5,6 @@ class Connector{
 	public:
 		virtual ~Connector(){}
 		virtual void sendTextMessage(const std::string& message)=0;
+		virtual void connect()=0;
+		//TODO virtual void disconnect()=0;?
 };
