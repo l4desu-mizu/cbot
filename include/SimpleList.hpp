37d888eccea04e9d615d1b4b0f9bb2b6ffb0c423
Mizu <mizu@blackicegames.de>
2017-07-19 11:13:13 +0200

    add SimpleList for Channels and Users

---
+++ include/SimpleList.hpp
@@ -0,0 +1,78 @@
+#pragma once
+#include "Entities.h"
+#include <list>
+
+template <class X>
+class SimpleList{
+	public:
+		SimpleList();
+		SimpleList(X& entity);
+		SimpleList(std::list<X>& entities);
+		~SimpleList();
+		bool contains(const X& entity);
+		void add(X entity);
+		void remove(const int id);
+		void remove(const X& entity);
+	private:
+		std::list<X> entities;
+};
+
+template <class X>
+inline
+SimpleList<X>::SimpleList(){}
+
+template <class X>
+inline
+SimpleList<X>::SimpleList(X& entity){
+	this->add(entity);
+}
+
+template <class X>
+inline
+SimpleList<X>::SimpleList(std::list<X>& entities){
+	this->entities=entities;
+}
+
+template <class X>
+inline
+SimpleList<X>::~SimpleList(){
+	entities.clear();
+}
+
+template <class X>
+inline
+bool SimpleList<X>::contains(const X& entity){
+	const auto it=std::find(entities.begin(), entities.end(), entity);
+	return ( it != entities.end());
+}
+
+template <class X>
+inline
+void SimpleList<X>::add(X entity){
+	entities.push_back(entity);
+}
+
+template <>
+inline
+void SimpleList<Entity>::remove(const int id){
+	for(auto it=entities.begin();it!=entities.end();it++){
+		if(it->getID()==id){
+			entities.erase(it);
+		}
+	}
+}
+
+template <class X>
+inline
+void SimpleList<X>::remove(const int id){
+	entities.erase(entities.begin()+id);
+}
+
+template <class X>
+inline
+void SimpleList<X>::remove(const X& entity){
+	auto it=std::find(entities.begin(), entities.end(), entity);
+	if( it != entities.end()) {
+		entities.erase(it);
+	}
+}
