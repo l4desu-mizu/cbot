c4f202f70d7db8be2d6c695a01c6a4a53c03d253
Mizu <mizu@blackicegames.de>
2017-07-19 15:13:12 +0200

    add listerner add functions to connector
    for message passing

--- include/Connector.h
+++ include/Connector.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "EntityListener.h"
+#include "TextListener.h"
 #include <string>
 
 class Connector{
@@ -6,5 +8,7 @@ class Connector{
 		virtual ~Connector(){}
 		virtual void sendTextMessage(const std::string& message)=0;
 		virtual void connect()=0;
+		virtual void addChannelListener(EntityListener* l)=0;
+		virtual void addUserListener(EntityListener* l)=0;
 		//TODO virtual void disconnect()=0;?
 };
