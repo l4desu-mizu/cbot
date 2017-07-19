18c010246b900629550edd1ca140057f955f2460
Mizu <mizu@blackicegames.de>
2017-07-19 17:15:34 +0200

    add missing header for Bot

---
+++ include/TextListener.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Entities.h"
+#include <string>
+
+struct Text{
+	std::string message;
+	const Entity& from;
+	const Entity& to;
+};
+class TextListener{
+	public:
+		virtual ~TextListener(){}
+		virtual void notify(const Text& text)=0;
+};
