9b84ea326a92ad91cd36df2768450b01f39bd0a1
Mizu <mizu@blackicegames.de>
2017-07-19 15:56:28 +0200

    fix entity constructor signature

--- src/Entities.cpp
+++ src/Entities.cpp
@@ -1,7 +1,7 @@
 #include "Entities.h"
 #include <algorithm>
 
-Entity::Entity(int id,std::string name, EntityType type, const bool myConcern):id(id),name(name),type(type),myConcern(myConcern){
+Entity::Entity(const int id,const std::string name,const  EntityType type, const bool myConcern):id(id),name(name),type(type),myConcern(myConcern){
 }
 bool Entity::operator==(const Entity& otherEntity) const{
 	return this->id==otherEntity.getID()&&this->type==otherEntity.getType();
