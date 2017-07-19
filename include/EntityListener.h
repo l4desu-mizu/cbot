12616c57746eaf5b8ea79faa30f801df89f91fc2
Mizu <mizu@blackicegames.de>
2017-07-19 14:23:24 +0200

    remove entitys not IDs
    unambiguous

--- include/EntityListener.h
+++ include/EntityListener.h
@@ -5,6 +5,6 @@
 class EntityListener{
 	public:
 		virtual ~EntityListener(){}
-		virtual void notify(const Entity& c)=0;
-		virtual void unnotify(const int id)=0;
+		virtual void notify(const Entity& e)=0;
+		virtual void unnotify(const Entity& e)=0;
 };
