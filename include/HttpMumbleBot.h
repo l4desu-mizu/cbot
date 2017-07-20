9bf3311b805323d5f7d9fdbe1f04a44ea3431c64
Mizu <mizu@blackicegames.de>
2017-07-20 14:11:51 +0200

    add HttpMumbleBot
    relays mumble messages and responds

---
+++ include/HttpMumbleBot.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Bot.h"
+#include "extensions/http/HttpRelay.h"
+
+class HttpMumbleBot:public Bot{
+	public:
+		HttpMumbleBot(Connector* connection, std::string url, std::string prefferedChannel);
+		~HttpMumbleBot();
+		void preRun();
+	protected:
+		bool respond(const Text& text);
+	private:
+		const std::string prefferedChannel;
+		HttpRelay* relay=NULL;
+};
+
---
+++ src/HttpMumbleBot.cpp
@@ -0,0 +1,35 @@
+#include "HttpMumbleBot.h"
+#include <vector>
+
+HttpMumbleBot::HttpMumbleBot(Connector* connection, std::string url, std::string prefferedChannel):
+Bot(connection),
+prefferedChannel(prefferedChannel){
+	relay=new HttpRelay(url);
+}
+HttpMumbleBot::~HttpMumbleBot(){
+	if(relay!=NULL){
+		delete relay;
+		relay=NULL;
+	}
+}
+void HttpMumbleBot::preRun(){
+	Bot::preRun();
+	std::vector<Channel> chan=channels.getCurrent();
+	for(auto it=chan.begin();it!=chan.end();it++){
+		if(it->getName()==prefferedChannel){
+			connection->moveToTextChat(*it);
+			break;
+		}
+	}
+}
+bool HttpMumbleBot::respond(const Text& text){
+	Text response=relay->relayMessage(text);
+	if(response.isPrivate){
+		std::vector<User> users;
+		users.push_back(text.from);
+		connection->whisperTextMessage(users,response.message);
+	}else{
+		connection->sendTextMessage(response.message);
+	}
+	return true;
+}
