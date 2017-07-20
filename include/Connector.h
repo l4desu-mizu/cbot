650b36d4f866fbc92bf5f171d3dab3e86a4dab38
Mizu <mizu@blackicegames.de>
2017-07-20 18:28:52 +0200

    add disconnect to Connector

--- include/Connector.h
+++ include/Connector.h
@@ -10,12 +10,12 @@ class Connector{
 	public:
 		virtual ~Connector(){}
 		virtual void connect()=0;
+		virtual void disconnect()=0;
 		virtual void updateUserInfo(const User& u)=0;
 		virtual void moveToTextChat(const Entity& c)=0;
 		virtual void sendTextMessage(const std::string& message)=0;
 		virtual void whisperTextMessage(const std::vector<User>& users, const std::string& message)=0;
 		virtual void whisperTextMessage(const std::vector<Channel>& channels, const std::string& message)=0;
-		//TODO virtual void disconnect()=0;?
 
 		void addConnectionListener(ConnectionListener* l);
 		void addChannelListener(EntityListener* l);
